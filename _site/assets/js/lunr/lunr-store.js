6fae1703e0d3e0344b5242e7d270937d1cd1d9cf
Gajol <doug@nielsenfamily.ca>
2022-03-16 23:35:55 -0400

    Nightly night

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Github Actions",
         "excerpt":"Try to get the actions and the makefile to work on the gulp-generated MD file. The gulp-generated MD file is a version-controlled file in Github. The file is generated on the Linux server and pushed to github. The makefile generates the PDF, but pandoc does not process the markdown syntax....","categories": ["pandoc"],
         "tags": ["pandoc","github-actions"],
-        "url": "/pandoc/2022/03/04/github-pandoc.html",
+        "url": "/balance/pandoc/2022/03/04/github-pandoc.html",
         "teaser": null
       }]
